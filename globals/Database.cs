9684348874ef04bb5a3f47fc22dd62c25a88b69c
Jareth Harmon <jarethharmon@gmail.com>
2022-07-03 01:45:22 -0500

    initial setup

---
+++ globals/Database.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Security.Cryptography;
+using Alphaleonis.Win32.Filesystem;
+using LiteDB;
+
+public class Database : Node
+{
+
+	public override void _Ready() {}
+
+}
---
+++ scripts/main.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+func _input(event:InputEvent) -> void:
+	if event is InputEventKey:
+		if event.scancode == KEY_F8:
+			_notification(MainLoop.NOTIFICATION_WM_QUIT_REQUEST)
+
+func _notification(what) -> void:
+	if what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST or what == MainLoop.NOTIFICATION_CRASH or what == MainLoop.NOTIFICATION_WM_GO_BACK_REQUEST:
+		# database calls
+		# stop threads
+		# save settings
+		print_debug("exiting program")
+		get_tree().quit()
+
