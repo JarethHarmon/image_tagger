4388aa49c5ec2a6c68c705219591ced3d5ad5c42
Jareth Harmon <jarethharmon@gmail.com>
2022-07-11 03:47:03 -0500

    add logic for page number spinbox/label

--- scripts/page_buttons.gd
+++ scripts/page_buttons.gd
@@ -11,13 +11,17 @@ onready var last_button:Button = $last_page
 onready var page_label:Label = $vbox/page_label
 onready var page_buttons:HBoxContainer = $hbox
 
+onready var current_page_spinbox:SpinBox = $hbox/current_page
+onready var total_pages_label:Label = $hbox/total_pages
+
 var current_page:int = 1
 var max_pages:int = 7
 var use_arrows:bool = false
 
 func _ready() -> void: 
 	Signals.connect("max_pages_changed", self, "_max_pages_changed")
-	change_page(1)
+	change_page(1) # keep for now
+	_max_pages_changed(77) # only for testing
 	
 	if use_arrows:
 		prev_button.text = "  <  "
@@ -33,9 +37,13 @@ func change_page(page:int) -> void:
 	current_page = page
 	Signals.emit_signal("page_changed", page)
 	page_label.text = String(current_page) + " / " + String(max_pages)
+	current_page_spinbox.value = page
 	print(current_page)
 	
-func _max_pages_changed(page_count:int) -> void: max_pages = page_count
+func _max_pages_changed(page_count:int) -> void: 
+	max_pages = page_count
+	current_page_spinbox.max_value = page_count
+	total_pages_label.text = " / " + String(page_count)
 func _on_prev_page_pressed() -> void: 
 	prev_button.release_focus()
 	change_page(current_page-1)
