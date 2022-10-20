cb10afd0a34bf98cfa3a9fae761774be3ab49cac
Jareth Harmon <jarethharmon@gmail.com>
2022-10-20 03:36:15 -0500

    update python src

--- src/pil_save_thumbnail.py
+++ src/pil_save_thumbnail.py
@@ -1,4 +1,4 @@
-import sys
+import sys, io, base64
 from PIL import Image, ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
@@ -36,3 +36,44 @@ def save_thumbnail(im_path, sv_path, sv_type, sv_size):
     result = 7
     result = save_PIL_thumbnail(im_path, sv_path, sv_size, sv_type)
     return result
+
+def create_thumbnail(im_path, sv_type, sv_size):
+    try:
+        im = Image.open(im_path)
+        if sv_type == 'jpeg': 
+            if has_transparency(im): 
+                if im.mode != 'RGBA': im.convert('RGBA')
+                sv_type = 'png'
+            elif im.mode != 'RGB': im = im.convert('RGB')       
+        im.thumbnail((sv_size, sv_size))
+        bi = io.BytesIO()
+        im.save(bi, sv_type)
+        base64_str = str(base64.b64encode(bi.getvalue()))
+        base64_str = base64_str.replace('\'', '')
+        base64_str = base64_str[1:len(base64_str)]
+        if sv_type == 'png': return f'1?{base64_str}'
+        return f'0?{base64_str}'
+    except:
+        return f'-1?'
+
+def create_webp(im_path, sv_path, sv_type, sv_size):
+    try:
+        im = Image.open(im_path)
+        if sv_type == 'jpeg': 
+            if has_transparency(im): 
+                if im.mode != 'RGBA': im.convert('RGBA')
+                sv_type = 'png'
+            elif im.mode != 'RGB': im = im.convert('RGB')  
+            
+        im.thumbnail((sv_size, sv_size))
+        im.save(sv_path, 'webp')
+        
+        bi = io.BytesIO()
+        im.save(bi, sv_type)
+        base64_str = str(base64.b64encode(bi.getvalue()))
+        base64_str = base64_str.replace('\'', '')
+        base64_str = base64_str[1:len(base64_str)]
+        if sv_type == 'png': return f'1?{base64_str}'
+        return f'0?{base64_str}'
+    except:
+        return f'-1?'
