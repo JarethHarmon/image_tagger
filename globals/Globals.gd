0388456d4884e20ace871f3479884940a818a257
Jareth Harmon <jarethharmon@gmail.com>
2022-11-08 16:33:26 -0600

    remove gdnative addon
    + remove gdnative addon (for now; may make a new one in the future)
    + update version number

--- globals/Globals.gd
+++ globals/Globals.gd
@@ -136,18 +136,6 @@ func save_settings() -> void:
 	if e == OK: f.store_string(var2str(settings))
 	f.close()
 
-func get_komi_hash(path:String) -> String: 
-	var gob:Gob = Gob.new()
-	var komi:String = gob.get_komi_hash(path)
-	gob.queue_free()
-	return komi
-
-func get_sha512(path:String) -> String:
-	var gob:Gob = Gob.new()
-	var sha512:String = gob.get_sha512_hash(path)
-	gob.queue_free()
-	return sha512
-
 func get_sha256(path:String) -> String: return File.new().get_sha256(path)
 
 func get_file_name(path:String) -> String: 
