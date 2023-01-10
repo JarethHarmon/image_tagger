b1e6235c865ad395826e084d5469c091b20f97fd
Jareth Harmon <jarethharmon@gmail.com>
2023-01-09 23:30:16 -0600

    cleanup

--- src/Database/Querier.cs
+++ src/Database/Querier.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using ImageTagger.Metadata;
 using LiteDB;
 
 namespace ImageTagger.Database
