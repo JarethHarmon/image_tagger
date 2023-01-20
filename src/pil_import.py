232f6af709daa8b928c9f8f52ac9e5ff07202e7f
Jareth Harmon <jarethharmon@gmail.com>
2023-01-20 11:45:40 -0600

    update python script

--- src/pil_import.py
+++ src/pil_import.py
@@ -14,7 +14,6 @@ SIZE = 8
 def initialize(im_path, sv_path):
     num_frames = getattr(Image.open(im_path), "n_frames", 1)
     image = Image.open(sv_path)
-    image = ImageOps.exif_transpose(image)
     imageL = image.convert('L')
     
     avg_hash = calc_average_hash(imageL)
@@ -29,8 +28,8 @@ def save_webp_thumbnail(im_path, sv_path, sv_size):
     image = Image.open(im_path)
     num_frames = getattr(image, "n_frames", 1)
     image.thumbnail((sv_size, sv_size))
-    image.save(sv_path, 'webp')
     image = ImageOps.exif_transpose(image)
+    image.save(sv_path, 'webp')
     imageL = image.convert('L')
 
     avg_hash = calc_average_hash(imageL)
@@ -115,10 +114,10 @@ def calc_phash_simple(imageL):
 
 def calc_color_buckets(image):
     a = np.asarray(image.convert('RGBA'))[:,:,3]
-    divisor = int(math.sqrt(image.width * image.height))
-    alpha = (a < 127).sum() // divisor
-    aa = (a > 127)
-    
+    divisor = int((image.width * image.height) / 256)
+    alpha = (a < 255).sum() // divisor
+    aa = (a > 16)
+
     pixels = np.asarray(image.convert('HSV'))
     h = pixels[:,:,0]
     s = pixels[:,:,1]
@@ -126,15 +125,15 @@ def calc_color_buckets(image):
     
     ss = s >= 10
     vv = v >= 10
-    sv = np.logical_and(ss, vv)
+    sva = np.logical_and(np.logical_and(ss, vv), aa)
     
-    rr = np.logical_and(np.logical_or(h < 21, h > 234), sv)
-    gg = np.logical_and(np.logical_and(h > 63, h < 106), sv)
-    bb = np.logical_and(np.logical_and(h > 149, h < 191), sv)
+    rr = np.logical_and(np.logical_or(h < 21, h > 234), sva)
+    gg = np.logical_and(np.logical_and(h > 63, h < 106), sva)
+    bb = np.logical_and(np.logical_and(h > 149, h < 191), sva)
 
-    yy = np.logical_and(np.logical_and(h >= 21, h <= 63), sv)
-    cc = np.logical_and(np.logical_and(h >= 106, h <= 149), sv)
-    ff = np.logical_and(np.logical_and(h >= 191, h <= 234), sv)
+    yy = np.logical_and(np.logical_and(h >= 21, h <= 63), sva)
+    cc = np.logical_and(np.logical_and(h >= 106, h <= 149), sva)
+    ff = np.logical_and(np.logical_and(h >= 191, h <= 234), sva)
     
     red = rr.sum() // divisor
     green = gg.sum() // divisor
@@ -144,14 +143,29 @@ def calc_color_buckets(image):
     cyan = cc.sum() // divisor
     fuchsia  = ff.sum() // divisor
 
-    light = np.logical_and(np.logical_and(v > 75, s < 25), aa).sum() // divisor
-    dark = np.logical_and(v < 25, aa).sum() // divisor
+    saa = np.logical_and(aa, s < 33)
     
-    nn = np.logical_and(s >= 40, s < 70)
-    dd = np.logical_and(ss, s < 40)
+    light = np.logical_and(saa, v > 67).sum() // divisor
+    medium = np.logical_and(aa, np.logical_and(v <= 67, v >= 33)).sum() // divisor
+    dark = np.logical_and(aa, v < 33).sum() // divisor
     
-    vivid = np.logical_and(s >= 70, vv).sum() // divisor
-    neutral = np.logical_and(nn, vv).sum() // divisor
-    dull = np.logical_and(dd, vv).sum() // divisor
+    vivid = np.logical_and(aa, s > 67).sum() // divisor
+    neutral = np.logical_and(aa, np.logical_and(s <= 67, s >= 33)).sum() // divisor
+    dull = saa.sum() // divisor
+
+    #light = np.logical_and(np.logical_and(v > 75, s < 25), aa).sum() // divisor
+    #dark = np.logical_and(v < 25, aa).sum() // divisor
     
-    return f'{red}?{green}?{blue}?{yellow}?{cyan}?{fuchsia}?{vivid}?{neutral}?{dull}?{light}?{dark}?{alpha}'
+    #nn = np.logical_and(s >= 40, s < 70)
+    #dd = np.logical_and(ss, s < 40)
+    
+    #vivid = np.logical_and(s >= 70, vv).sum() // divisor
+    #neutral = np.logical_and(nn, vv).sum() // divisor
+    #dull = np.logical_and(dd, vv).sum() // divisor
+    return f'{red}?{green}?{blue}?{yellow}?{cyan}?{fuchsia}?{vivid}?{neutral}?{dull}?{light}?{medium}?{dark}?{alpha}'
+
+#image = Image.open('W:/images/16176b646fd9c33f3444b6850ea67d3a.jpg')
+#image.thumbnail((256, 256))
+#image = ImageOps.exif_transpose(image)
+#image.save('W:/test_1.png', 'png')
+#print(calc_color_buckets(image))
